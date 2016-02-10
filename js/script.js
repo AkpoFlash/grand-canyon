7e8410bafcdf66bd81d3ac8ed73992bb1b651331
AkpoFlash <akpoflash@gmail.com>
2016-02-10 19:05:12 +0300

    Add landing element

--- js/script.js
+++ js/script.js
@@ -1,10 +1,26 @@
+var wScroll = $(this).scrollTop();
+
+function showImage(wScroll){
+	if(wScroll > $(".tours-pics").offset().top){
+		$(".tours-pics figure").each(function(i,elem){
+			setTimeout(function(){
+				$(".tours-pics figure").eq(i).addClass("is-show");
+			}, 200 * (i + 1));
+		});
+	}
+}
+
 $(document).on("scroll",function(){
-	var wScroll = $(this).scrollTop();
-	console.log(wScroll);
+	wScroll = $(this).scrollTop();
 	$(".logo").css({
 		"transform":"translate(0,"+wScroll/2+"%)"
 	});
 	$(".bird").css({
 		"transform":"translate(0,-"+wScroll/30+"%)"
 	});
+	showImage(wScroll);
+});
+
+$(document).ready(function(){
+	showImage(wScroll);
 });
