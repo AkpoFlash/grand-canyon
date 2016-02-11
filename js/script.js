63af0e1747ee8687c6c1e88b7415358e38557431
AkpoFlash <akpoflash@gmail.com>
2016-02-11 18:19:37 +0300

    Add popup blogs

--- js/script.js
+++ js/script.js
@@ -19,6 +19,21 @@ $(document).on("scroll",function(){
 		"transform":"translate(0,-"+wScroll/40+"%)"
 	});
 	showImage(wScroll);
+
+	if(wScroll > $(".promo").offset().top - $(window).height()){
+		var opacity = (wScroll - $(".promo").offset().top + $(window).height()) / $(window).height();
+		$(".promo").css({"opacity":opacity});
+	}
+
+	if(wScroll > $(".blogs").offset().top - $(window).height()){
+		var offset = wScroll - $(".blogs").offset().top + $(window).height() / 3;
+		$(".blog-1").css({
+			"transform": "translate("+ Math.min(0,offset) + "px," + -Math.min(0,offset)/2 + "px)" 
+		});
+		$(".blog-3").css({
+			"transform": "translate("+ -Math.min(0,offset) + "px," + -Math.min(0,offset)/2 + "px)" 
+		});
+	}
 });
 
 $(document).ready(function(){
